91134aeaded792f913f55ad4d0e24a3adfc81715
Noah Murphy <ncm0897@gmail.com>
2024-09-05 20:43:24 -0700

    all done here

---
+++ exercise5.py
@@ -0,0 +1,14 @@
+# Exercise 5: Sum to N
+#
+# Write a function named `sum_to` that takes a single integer n and returns the sum of all integers from 1 to n.
+#
+# Examples:
+# sum_to(6) should return 21.
+# sum_to(10) should return 55.
+#
+# Define the function and then call it below.
+def sum_to(n):
+    return n * (n + 1) // 2
+
+
+print('Exercise 5:', sum_to(6))
